6bdd31659044da43e44692eb5d74d08d27ecde4a
vima91 <evram.ehab@gmail.com>
2019-11-20 11:58:49 +0200

    - Code cleanup. - Added references for Esri map, Kendo grid and Kendo chart control. - Implemented Esri map. - Implemented Search for Esri map. - Implemented default chart. - Implemented default grid.

--- EsriTask/Pages/Index.cshtml.cs
+++ EsriTask/Pages/Index.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,9 +16,36 @@ namespace EsriTask.Pages
             _logger = logger;
         }
 
-        public void OnGet()
-        {
+        //public async Task<JsonResult> OnGet()
+        //{
+        //    string responseBody;
+        //    using (var client = new HttpClient())
+        //    {
+        //        try
+        //        {
+        //            //Would best be in appSettings, structured as base url, api version then service/name
+        //            var apiUrl =
+        //                "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/5?f=json&pretty=true";
 
-        }
+        //            var response = await client.GetAsync(apiUrl);
+        //            response.EnsureSuccessStatusCode();
+        //            responseBody = await response.Content.ReadAsStringAsync();
+        //            //Convert to strong type here
+        //        }
+        //        catch (HttpRequestException e)
+        //        {
+        //            //Some logging goes here, then handling
+        //            _logger.Log(LogLevel.Error, e, "Http Request Exception Occured");
+        //            throw;
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            //Some more generic logging goes here, then generic handling
+        //            _logger.Log(LogLevel.Error, e, "Generic Exception Occured");
+        //            throw;
+        //        }
+        //    }
+        //    return new JsonResult(responseBody);
+        //}
     }
 }
--- EsriTask/Startup.cs
+++ EsriTask/Startup.cs
@@ -15,13 +15,11 @@ namespace EsriTask
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -31,17 +29,13 @@ namespace EsriTask
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
