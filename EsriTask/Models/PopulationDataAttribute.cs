817c39a3e08ada62ad7511e8856891a2a16e7184
vima91 <evram.ehab@gmail.com>
2019-11-21 19:53:13 +0200

    - Modified chart. - Modified grid. - Added model to hold fetched census data. - Added fetching for real data. - Added logic to extract only needed census data. - Added Newtonsoft.Json.

---
+++ EsriTask/Models/PopulationDataAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace EsriTask.Models
+{
+    public class PopulationDataAttribute
+    {
+        public PopulationDataAttribute(string attrib, int population)
+        {
+            Attrib = attrib;
+            Population = population;
+        }
+
+        public string Attrib { get; set; }
+        public int Population { get; set; }
+    }
+}
--- EsriTask/Pages/Index.cshtml.cs
+++ EsriTask/Pages/Index.cshtml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using EsriTask.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 
 namespace EsriTask.Pages
 {
@@ -16,36 +19,53 @@ namespace EsriTask.Pages
             _logger = logger;
         }
 
-        //public async Task<JsonResult> OnGet()
-        //{
-        //    string responseBody;
-        //    using (var client = new HttpClient())
-        //    {
-        //        try
-        //        {
-        //            //Would best be in appSettings, structured as base url, api version then service/name
-        //            var apiUrl =
-        //                "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/5?f=json&pretty=true";
 
-        //            var response = await client.GetAsync(apiUrl);
-        //            response.EnsureSuccessStatusCode();
-        //            responseBody = await response.Content.ReadAsStringAsync();
-        //            //Convert to strong type here
-        //        }
-        //        catch (HttpRequestException e)
-        //        {
-        //            //Some logging goes here, then handling
-        //            _logger.Log(LogLevel.Error, e, "Http Request Exception Occured");
-        //            throw;
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            //Some more generic logging goes here, then generic handling
-        //            _logger.Log(LogLevel.Error, e, "Generic Exception Occured");
-        //            throw;
-        //        }
-        //    }
-        //    return new JsonResult(responseBody);
-        //}
+        public async Task<JsonResult> OnGetPopulationData(string searchTerm)
+        {
+            var attributes = new List<PopulationDataAttribute>();
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    //Would best be in appSettings, structured as base url, api version then service/name/location, then query string items
+                    var baseUrl = "http://sampleserver1.arcgisonline.com";
+                    var api = "ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/find";
+                    var options =
+                        "layers=5&returnGeometry=false&f=pjson";
+                    var apiUrl =
+                        $"{baseUrl}/{api}?searchText={searchTerm}&{options}";
+
+                    var response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    
+                    if (!string.IsNullOrEmpty(responseBody))
+                    {
+                        dynamic populationData = JObject.Parse(responseBody);
+                        foreach (var attribute in populationData.results[0].attributes)
+                        {
+                            //Only interested in certain attributes
+                            if (attribute.Name.ToString().StartsWith("AGE_"))
+                            {
+                                attributes.Add(new PopulationDataAttribute(attribute.Name, Convert.ToInt32(attribute.Value)));
+                            }
+                        }
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    //Some logging goes here, then handling
+                    _logger.Log(LogLevel.Error, e, "Http Request Exception Occured");
+                    throw;
+                }
+                catch (Exception e)
+                {
+                    //Some more generic logging goes here, then generic handling
+                    _logger.Log(LogLevel.Error, e, "Generic Exception Occured");
+                    throw;
+                }
+            }
+            return new JsonResult(attributes);
+        }
     }
 }
